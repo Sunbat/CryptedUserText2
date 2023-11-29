907062c537211d8c0f5e00bb8a0ab07791f8a261
Kabouu <loicrulin@gmail.com>
2023-11-28 20:40:56 -0500

    First commit

---
+++ User.java
@@ -0,0 +1,14 @@
+class User{
+    String username;
+    String firstName;
+    String lastName;
+    String password;
+    
+    public User(user, pass, first, last){
+        username = user;
+        password = pass;
+        firstName = first;
+        lastName = last;
+
+    }
+}
